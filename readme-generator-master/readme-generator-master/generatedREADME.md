c56a763e26e238e66dd618d40e82cde3f8dd2c92
1066JMay96 <williammay.03@gmail.com>
2020-08-25 21:23:00 -0400

    replaced the previous files

---
+++ readme-generator-master/readme-generator-master/generatedREADME.md
@@ -0,0 +1,59 @@
+
+# readme-generator
+
+
+## Description
+This generates a readme from your github profile
+
+## Table of Contents
+
+[Installation](#installation) 
+
+[Usage](#usage) 
+
+[License](#license) 
+
+[Contributing](#contributing) 
+
+[Tests](#tests) 
+
+[Badges](#badges) 
+
+[Information](#information) 
+
+
+## Installation
+
+this gets install by cloning it, than downloading node axios and inquirer
+
+## Usage
+
+by typing in node index.js
+
+## License
+
+
+
+## Contributing
+
+William May
+
+## Tests
+
+I tested it myself
+
+## Badges
+
+![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)
+
+## Information
+
+![GitHub Profile Picture](https://avatars1.githubusercontent.com/u/56660247?v=4 "GitHub Profile Picture") 
+
+William May 
+
+[1066JMay96](https://github.com/1066JMay96) 
+
+williammay.03@gmail.com 
+
+  
---
+++ readme-generator-master/readme-generator-master/lib/generate-readme.js
@@ -0,0 +1,53 @@
+const generateReadMe = profileData => {
+  const licenseString = profileData.license.join(", ");
+  return `
+# ${profileData.title}
+
+
+## Description
+${profileData.description}
+
+## Table of Contents
+
+[Installation](#installation) \n
+[Usage](#usage) \n
+[License](#license) \n
+[Contributing](#contributing) \n
+[Tests](#tests) \n
+[Badges](#badges) \n
+[Information](#information) \n
+
+## Installation
+
+${profileData.installation}
+
+## Usage
+
+${profileData.usage}
+
+## License
+
+${licenseString}
+
+## Contributing
+
+${profileData.contributing}
+
+## Tests
+
+${profileData.tests}
+
+## Badges
+
+![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)
+
+## Information
+
+![GitHub Profile Picture](${profileData.githubData.data.avatar_url} "GitHub Profile Picture") \n
+${profileData.githubData.data.name} \n
+[${profileData.username}](https://github.com/${profileData.username}) \n
+${profileData.email} \n
+  `
+};
+
+module.exports = generateReadMe;
